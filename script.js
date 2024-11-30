35bca6f052c5177d945656534c6444c04e9e74fd
Makayla Wray <mwray1@my.westga.edu>
2024-11-30 15:47:46 -0500

    Add responsive mobile nav

--- script.js
+++ script.js
@@ -9,3 +9,14 @@ function redirectToResources(){
 function redirectToAbout(){
     window.location.href = "about.html";
 }
+
+const hamMenu = document.querySelector('.ham-menu');
+const bodyScroll =document.querySelector('body');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+hamMenu.addEventListener('click', () => {
+    hamMenu.classList.toggle('active');
+    mobileMenu.classList.toggle('active');
+    bodyScroll.classList.toggle('active');
+
+})
